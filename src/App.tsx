715ec2e957b228a82aad32d60d8287b26d4233e9
aram <julio.santacruzh@gmail.com>
2024-07-22 09:45:34 -0700

    hide dev mode in production

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,7 @@ import AsideModal from "./components/AsideModal";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
-  const { theRules, stats,  handleKeyup, fetchData } =
-    useGameStore();
+  const { theRules, stats, handleKeyup, fetchData } = useGameStore();
   const { isFirstTime } = useWordleStore();
 
   const currentWord = useGameStore((state) => state.word);
@@ -19,6 +18,8 @@ function App() {
 
   const currenGuess = useGameStore((state) => state.currentGuess);
 
+  const isDev = import.meta.env.VITE_IS_DEV==='true'?true:false;
+  console.log(typeof(isDev))
   useEffect(() => {
     fetchData();
 
@@ -31,41 +32,40 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-
-
   return (
-
-
-        <>
-          <div className="max-w-[520px] h-screen w-screen mx-auto flex flex-col items-center justify-between bg-[#F9F9F9] dark:bg-[#262B3C]">
-            <Header darkMode={darkMode} setDarkMode={setDarkMode} />
-            <div className="w-full flex flex-col items-center my-[50px] px-2   ">
-              {guessesArray.map((_, index) => {
-                return (
-                  <GuessRow
-                    key={"guess" + index}
-                    guess={guessesArray[index]}
-                    word={currentWord}
-                    isGuessed={index < currenGuess}
-                  />
-                );
-              })}
-            </div>
+    <>
+      <div className="max-w-[520px] h-screen w-screen mx-auto flex flex-col items-center justify-between bg-[#F9F9F9] dark:bg-[#262B3C]">
+        <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+        <div className="w-full flex flex-col items-center my-[50px] px-2   ">
+          {guessesArray.map((_, index) => {
+            return (
+              <GuessRow
+                key={"guess" + index}
+                guess={guessesArray[index]}
+                word={currentWord}
+                isGuessed={index < currenGuess}
+              />
+            );
+          })}
+        </div>
+        {isDev ? (
+          <>
             <p>{currentWord}</p>
             <p>{JSON.stringify(guessesArray)}</p>
-            <Keyboard darkMode={darkMode} setDarkMode={setDarkMode} />
-          </div>
+          </>
+        ): null}
 
-          {theRules || isFirstTime ? (
-            <AsideModal
-              children={<TheRules />}
-              openModal={theRules || isFirstTime}
-            />
-          ) : null}
-          {stats ? <AsideModal children={<Stats />} openModal={stats} /> : null}
-        </>
+        <Keyboard darkMode={darkMode} setDarkMode={setDarkMode} />
+      </div>
 
-
+      {theRules || isFirstTime ? (
+        <AsideModal
+          children={<TheRules />}
+          openModal={theRules || isFirstTime}
+        />
+      ) : null}
+      {stats ? <AsideModal children={<Stats />} openModal={stats} /> : null}
+    </>
   );
 }
 
